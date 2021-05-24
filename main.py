2c15ec3d53eacb17d91b77d4e25488d2d6418573
jmasonlee <jmasonlee@gmail.com>
2021-05-24 09:32:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! Miramichi'
+    return 'Cain was the first man Abel to kill'
 
 @app.route('/echo/<name>')
 def echo(name):
